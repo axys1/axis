4ad4b7579b11aeae14f261bc1b3b459fd326b795
rh101 <rh101@users.noreply.github.com>
2025-02-17 14:14:48 +1100

    Fix line positioning due to line spacing

--- core/2d/Label.cpp
+++ core/2d/Label.cpp
@@ -1756,10 +1756,10 @@ void Label::updateContent()
         if (_numberOfLines > 0 && _currentLabelType != LabelType::STRING_TEXTURE)
         {
             // This is the logic for TTF fonts
-            float thickness         = (_lineHeight * _fontScale) * 0.12f;
-            float nextTokenY        = _letterOffsetY;
+            float thickness         = std::max((_lineHeight * _fontScale) * 0.12f, 2.f);
+            float nextY             = _letterOffsetY;
             auto contentScaleFactor = AX_CONTENT_SCALE_FACTOR();
-            float lineSpacing       = _lineSpacing * contentScaleFactor;
+            float lineSpacing       = _lineSpacing;
             float lineHeight        = _lineHeight * _fontScale / contentScaleFactor;
 
             // atlas font
@@ -1767,19 +1767,19 @@ void Label::updateContent()
             {
                 if (_strikethroughEnabled)
                 {
-                    auto y = nextTokenY - lineHeight / 2;
+                    auto y = nextY - lineHeight / 2;
                     _lineDrawNode->drawLine(Vec2(_linesOffsetX[i], y), Vec2(_linesWidth[i] + _linesOffsetX[i], y),
                                             Color4F(lineColor), thickness);
                 }
 
                 if (_underlineEnabled)
                 {
-                    auto y = nextTokenY - lineHeight;
+                    auto y = nextY - lineHeight;
                     _lineDrawNode->drawLine(Vec2(_linesOffsetX[i], y), Vec2(_linesWidth[i] + _linesOffsetX[i], y),
                                             Color4F(lineColor), thickness);
                 }
 
-                nextTokenY -= lineHeight + lineSpacing;
+                nextY -= lineHeight + lineSpacing;
             }
         }
         else if (_textSprite) // ...and is the logic for System fonts
@@ -1789,8 +1789,8 @@ void Label::updateContent()
 
             // FIXME: system fonts don't report the height of the font correctly. only the size of the texture, which is POT
             // FIXME: Might not work with different vertical alignments
-            const auto thickness = spriteSize.height / static_cast<float>(_numberOfLines) * 0.12f;
             const auto lineSize  = spriteSize.height / static_cast<float>(_numberOfLines);
+            const auto thickness = std::max(lineSize * 0.12f, 2.f);
 
             if (_underlineEnabled)
             {
@@ -1804,7 +1804,8 @@ void Label::updateContent()
 
             if (_strikethroughEnabled)
             {  
-                const auto baseY    = lineSize - lineSize / 2;
+                const auto baseY = lineSize - lineSize / 2;
+                
                 for (int i = 0; i < _numberOfLines; ++i)
                 {
                     float y = baseY + lineSize * i;
